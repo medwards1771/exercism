a74ac3fdff19e8081bafd82730a20041be8eb6ec
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-04-01 17:36:49 -0400

    First solution for rna-transcription

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,31 @@
+class Complement
+  def self.of_dna(nucleotide_string)
+    return '' if self.contaminated(nucleotide_string)
+    nucleotide_string.chars.map do |char|
+      get_complementary_nuke(char)
+    end.join
+  end
+
+  def self.get_complementary_nuke(nucleotide)
+    if nucleotide == 'G'
+      'C'
+    elsif nucleotide == 'C'
+      'G'
+    elsif nucleotide == 'T'
+      'A'
+    elsif nucleotide == 'A'
+      'U'
+    end
+  end
+
+  def self.contaminated(string)
+    clean_string = string.chars.select do |char|
+      ['G', 'C', 'T', 'A'].include?(char)
+    end.join
+    clean_string != string
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
