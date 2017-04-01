91f878b3619e89ba0a8540bcac4e3b45e3913aef
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-04-01 16:37:07 -0400

    First solution for hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,4 +1,14 @@
 class Hamming
-  def self.compute(_, _)
+  def self.compute(first_string, second_string)
+    raise ArgumentError if first_string.length != second_string.length
+    difference = 0
+    while first_string != ''
+      difference +=1 if first_string.slice!(0) != second_string.slice!(0)
+    end
+    difference
   end
+end
+
+module BookKeeping
+  VERSION = 3
 end
