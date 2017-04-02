dd17a4a1a654715122f62424974a4b753bee65a6
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-04-02 11:21:26 -0400

    First solution for pangram

---
+++ ruby/pangram/pangram.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class Pangram
+  def self.pangram?(phrase)
+    alphabet_letters = ("a".."z").to_a
+    clean_phrase = phrase.downcase.tr('^a-z', '')
+    clean_phrase.chars.each{ |char| alphabet_letters.delete(char) }
+    alphabet_letters == []
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
