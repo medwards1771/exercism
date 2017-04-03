c1a78d4afa74ae829a7f2525c68e63ada8ead1bd
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-04-03 16:39:55 -0400

    Trying to extract similarities

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,19 +1,25 @@
+# Questions
+# 1. when do you know when the transformation from fixnum to roman numeral is complete??
+  # depends on how big the number is
+
 class Fixnum
   def to_roman
-    ones = "I" * self
-    fives = ones.gsub("IIIII", "V")
-    tens = fives.gsub("VV", "X")
-    fifties = tens.gsub("XXXXX", "L")
-    hundreds = fifties.gsub("LL", "C")
-    five_hundreds = hundreds.gsub("CCCCC", "D")
-    thousands = five_hundreds.gsub("DD", "M")
+    answer = "I" * self
+    answer = answer.gsub("IIIII", "V") if self > 4
+    answer = answer.gsub("VV", "X") if self > 9
+    answer = answer.gsub("XXXXX", "L") if self > 49
+    answer = answer.gsub("LL", "C") if self > 99
+    answer = answer.gsub("CCCCC", "D") if self > 499
+    answer = answer.gsub("DD", "M") if self > 999
 
-    answer = thousands.gsub("VIIII", "IX") # 9 is 1 less than 10
-    answer2 = answer.gsub("IIII", "IV") # 4 is 1 less than 5
-    answer3 = answer2.gsub("XXXX", "XL") # 40 is 10 less than 50
-    answer4 = answer3.gsub("LXL","XC") # 90 is 10 less than 100
-    answer5 = answer4.gsub("CCCC", "CD") # 400 is 100 less than 500
-    answer6 = answer5.gsub("DCD", "CM") # 900 is 100 less than 1000
+    answer = answer.gsub("IIII", "IV") if answer.include?("IIII") # 4 is 1 less than 5
+    answer = answer.gsub("VIV", "IX") if answer.include?("VIV") # 9 is 1 less than 10
+    answer = answer.gsub("XXXX", "XL") if answer.include?("XXXX") # 40 is 10 less than 50
+    answer = answer.gsub("LXL","XC") if answer.include?("LXL") # 90 is 10 less than 100
+    answer = answer.gsub("CCCC", "CD") if answer.include?("CCCC") # 400 is 100 less than 500
+    answer = answer.gsub("DCD", "CM") if answer.include?("DCD") # 900 is 100 less than 1000
+
+    answer
   end
 end
 
