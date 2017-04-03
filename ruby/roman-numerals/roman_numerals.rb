7f2c8f374bd6e2415638ffbd060680ffe33c0b96
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-04-03 15:53:17 -0400

    Reorder and add comments for clarity

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -7,12 +7,13 @@ class Fixnum
     hundreds = fifties.gsub("LL", "C")
     five_hundreds = hundreds.gsub("CCCCC", "D")
     thousands = five_hundreds.gsub("DD", "M")
-    answer = thousands.gsub("XXXX", "XL")
-    answer2 = answer.gsub("LXL","XC")
-    answer3 = answer2.gsub("CCCC", "CD")
-    answer4 = answer3.gsub("DCD", "CM")
-    answer5 = answer4.gsub("VIIII", "IX")
-    answer6 = answer5.gsub("IIII", "IV")
+
+    answer = thousands.gsub("VIIII", "IX") # 9 is 1 less than 10
+    answer2 = answer.gsub("IIII", "IV") # 4 is 1 less than 5
+    answer3 = answer2.gsub("XXXX", "XL") # 40 is 10 less than 50
+    answer4 = answer3.gsub("LXL","XC") # 90 is 10 less than 100
+    answer5 = answer4.gsub("CCCC", "CD") # 400 is 100 less than 500
+    answer6 = answer5.gsub("DCD", "CM") # 900 is 100 less than 1000
   end
 end
 
