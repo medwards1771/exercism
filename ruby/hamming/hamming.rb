e0dd1c0040346ba3e6268ecfa5e6153c62bfe851
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-04-08 15:59:51 -0400

    Use enumerator rather than a loop

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,11 +1,9 @@
 class Hamming
-  def self.compute(first_string, second_string)
-    raise ArgumentError if first_string.length != second_string.length
-    difference = 0
-    while first_string != ''
-      difference += 1 if first_string.slice!(0) != second_string.slice!(0)
+  def self.compute(strand1, strand2)
+    raise ArgumentError if strand1.length != strand2.length
+    strand1.each_char.with_index.count do |char, index|
+      char != strand2[index]
     end
-    difference
   end
 end
 
