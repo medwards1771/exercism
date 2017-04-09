951296d19e671f02cd030724e6420b32afdaf05a
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-04-09 11:29:11 -0400

    Refactor for concision

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -4,33 +4,16 @@ class Phrase
   end
 
   def word_count
-    grouped_words = clean_words.group_by { |word| word }
-    grouped_words.map do |word, appearances|
-      grouped_words[word] = appearances.size
-    end
-    grouped_words
+    Hash.new(0).tap { |h| clean_string.split.each { |word| h[word] += 1 } }
   end
 
   private
 
   attr_reader :word_string
 
-  def clean_words
-    clean_string.split.map do |word|
-      if word[0] == "'" && word[-1] == "'"
-        remove_single_quotes(word)
-      else
-        word
-      end
-    end
-  end
-
   def clean_string
-    word_string.downcase.gsub(/[^a-z0-9'\s]/, ' ')
-  end
-
-  def remove_single_quotes(string)
-    string.delete("'")
+    half_clean_string = word_string.downcase.gsub(/[^a-z0-9'\s]/, ' ')
+    half_clean_string.gsub(" '", ' ').gsub("' ", ' ')
   end
 end
 
