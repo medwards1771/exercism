39ee09344c03ee5a372850d894fb267fc1de20e5
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-04-09 11:52:32 -0400

    Remove redundant logic

--- ruby/bracket-push/bracket_push.rb
+++ ruby/bracket-push/bracket_push.rb
@@ -1,58 +1,48 @@
 class Brackets
-  def self.paired?(string)
-    return true if string == ''
-    return false if begins_with_closer?(string[0])
-    string = clean_up(string)
-    return false unless string.delete('').size.even?
-    return true if matching?(string) && nested?(string)
-  end
+  class << self
+    def paired?(string)
+      return true if string == ''
+      return false if begins_with_closer?(string[0])
+      matching?(clean_up(string)) && nested?(clean_up(string))
+    end
 
-  def self.clean_up(string)
-    string.split(//).select do |char|
-      ['{', '}', '[', ']', '(', ')'].include?(char)
-    end.join
-  end
+    private
 
-  def self.begins_with_closer?(char)
-    char == '}' || char == ']' || char == ')'
-  end
+    def clean_up(string)
+      string.split(//).select do |char|
+        ['{', '}', '[', ']', '(', ')'].include?(char)
+      end.join
+    end
 
-  def self.matching?(string)
-    string = string.tr('{', '(')
-    string = string.tr('[', '(')
-    string = string.tr('}', ')')
-    string = string.tr(']', ')')
-    string.count('(') == string.count(')')
-  end
+    def begins_with_closer?(char)
+      char == '}' || char == ']' || char == ')'
+    end
 
-  def self.nested?(string)
-    get_sandwich(string)
-  end
+    def matching?(string)
+      string = string.tr('{', '(')
+      string = string.tr('[', '(')
+      string = string.tr('}', ')')
+      string = string.tr(']', ')')
+      string.count('(') == string.count(')')
+    end
 
-  def self.get_sandwich(string)
-    first_char_closer_index = find_last_possible_closer_index(string)
-    sandwich = string[0, first_char_closer_index + 1]
-    matching?(sandwich)
-  end
+    def nested?(string)
+      get_sandwich(string)
+    end
 
-  def self.find_last_possible_closer_index(string)
-    if string[0] == '['
-      if string.count(']') > 1
+    def get_sandwich(string)
+      first_char_closer_index = find_last_possible_closer_index(string)
+      sandwich = string[0, first_char_closer_index + 1]
+      matching?(sandwich)
+    end
+
+    def find_last_possible_closer_index(string)
+      if string[0] == '['
         string.rindex(']')
-      else
-        string.index(']')
-      end
-    elsif string[0] == '{'
-      if string.count('}') > 1
+      elsif string[0] == '{'
         string.rindex('}')
-      else
-        string.index('}')
-      end
-    elsif string[0] == '('
-      if string.count(')') > 1
+      elsif string[0] == '('
         string.rindex(')')
-      else
-        string.index(')')
       end
     end
   end
