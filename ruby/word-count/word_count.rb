ea6a8b3ffe716311ea16ed7a574eac9875ef733c
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-04-09 19:02:18 -0400

    Improve naming

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,19 +1,19 @@
 class Phrase
-  def initialize(word_string)
-    @word_string = word_string
+  def initialize(phrase)
+    @phrase = phrase
   end
 
   def word_count
-    Hash.new(0).tap { |h| clean_string.split.each { |word| h[word] += 1 } }
+    Hash.new(0).tap { |h| clean.split.each { |word| h[word] += 1 } }
   end
 
   private
 
-  attr_reader :word_string
+  attr_reader :phrase
 
-  def clean_string
-    half_clean_string = word_string.downcase.gsub(/[^a-z0-9'\s]/, ' ')
-    half_clean_string.gsub(" '", ' ').gsub("' ", ' ')
+  def clean
+    half_clean = phrase.downcase.gsub(/[^a-z0-9'\s]/, ' ')
+    half_clean.gsub(" '", ' ').gsub("' ", ' ')
   end
 end
 
