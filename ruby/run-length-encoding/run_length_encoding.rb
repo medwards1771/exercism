cbe1e3383a24ae6e0492cec9f01e634989b30fa4
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-04-13 15:06:54 -0400

    Comment on code in order to make refactoring easier

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -6,28 +6,31 @@ module RunLengthEncoding
   # you can either go forward or go backward!
 
   def encode(input)
-    i = 0
-    string = ''
-    while i < input.length
-      char = input[i]
-      answer = method(char, input, i, string)
-      i = answer[:index]
-      string = answer[:string]
+    i = 0 # set index to zero
+    string = '' # declare an empty string
+    while i < input.length # initiate a loop that ends at the last character of the input string
+      char = input[i] # assign char to the first character, then the second character, etc
+      answer = method(char, input, i, string) # send char (varies), input string, index, and empty string to method
+      # why does method need an empty string? it doesn't.
+      i = answer[:index] # retrieve the index from the hash passed by #method
+      string = answer[:string] #retrieve the string from the hash passed by #method
+      # okay so know i at least see a reason for passing string to method since it changes from ''
     end
-    answer[:string]
+    answer[:string] #return the final string passed by #method as the final answer.
   end
 
-  def method(char, input, index, string)
-    dummy = input
-    char = dummy[index]
-    char_count = 0
-    until dummy[index] != char
-      char_count += 1
-      index += 1
+  def method(char, input, index, string) # takes 4 values as parameters
+    dummy = input # stores input string as local var 'dummy'
+    char = dummy[index] # assigns char to the given index
+    # so we don't need to pass char I believe ...
+    char_count = 0 # set char_count to zero. essentially fxns as a timer
+    until dummy[index] != char # initiates a loop that ends when char no longer repeats itself in the string
+      char_count += 1 # increments the counter
+      index += 1 # increments the index
     end
-    char_count = '' if char_count == 1
-    string += "#{char_count}#{char}"
-    { string: string, index: index }
+    char_count = '' if char_count == 1 # don't show the char_count in the string if it's 1
+    string += "#{char_count}#{char}" # build the response string by adding stuff like "12W"
+    { string: string, index: index } # return a hash with the string in its current form and the current index
   end
 
   def decode(input)
