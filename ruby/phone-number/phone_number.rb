b88eea5de8b7c6c45f2e4a9bdf1aa203cc2c91e6
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-07-12 13:01:00 -0400

    Code breaks on #test_invalid_when_11_digits_does_not_start_with_a_1

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,28 +1,52 @@
 class PhoneNumber
   def self.clean(string)
     phone_number = PhoneNumberString.new(string)
-    phone_number.clean if phone_number.valid?
+    phone_number.just_digits if phone_number.valid?
   end
 end
 
+# write a blog post where you explain why you chose to make two different
+# classes, one for 11 char string and one for 10 char string because of code
+# in current state and esp. bc of this test:
+# #test_invalid_when_11_digits_does_not_start_with_a_1
+
+
+#  explain why you can't do
+# string = String.new('fox')
+# string => 'fox'
+# CANNOT do string.contents = 'ox' because there is no method a la
+#   #contents=
+# so you have create a string object
+# out of the string that's passed to PhoneNumberString
+# still curious to know what the Ruby method looks like for #initialize
+# when do you call a method on an object and when do you pass an object to a
+# method ?
+
 class PhoneNumberString < String
+  attr_accessor :string
+
   PHONE_NUMBER_PUNCTUATION_MARKS = ['(', ')', ' ', '-', '.', '+'].freeze
   DIGITS = ('0'..'9').to_a
   AREA_CODE_DIGITS = ('2'..'9').to_a
   EXCHANGE_CODE_DIGITS = ('2'..'9').to_a
   VALID_PHONE_NUMBER_CHARS = PHONE_NUMBER_PUNCTUATION_MARKS + DIGITS
 
-  def clean
-    if digit_count == 10
-      just_digits
-    elsif digit_count == 11
-      just_digits[1..-1]
-    end
+  def initialize(string)
+    @string = string
+    @string = standardize(string)
+  end
+
+  def standardize(string)
+    remove_eleventh_digit(string)
+  end
+
+  def remove_eleventh_digit(string)
+    just_digits.size == 11 ? just_digits[1..-1] : string
   end
 
   def valid?
     valid_area_code? && valid_exchange_code? && valid_length? &&
-      all_chars_valid? && valid_first_digit?
+       all_chars_valid? #&& valid_first_digit?
   end
 
   def valid_area_code?
@@ -34,35 +58,23 @@ class PhoneNumberString < String
   end
 
   def exchange_code
-    if digit_count == 10
-      just_digits[3]
-    elsif digit_count == 11
-      just_digits[4]
-    end
+    just_digits[3]
   end
 
   def area_code
-    if digit_count == 10
-      just_digits[0]
-    elsif digit_count == 11
-      just_digits[1]
-    end
+    just_digits[0]
   end
-
-  def valid_first_digit?
-    if digit_count == 11
-      first_digit == '1'
-    elsif digit_count == 10
-      AREA_CODE_DIGITS.include?(first_digit)
-    end
-  end
+  #
+  # def valid_first_digit?
+  #   AREA_CODE_DIGITS.include?(first_digit)
+  # end
 
   def just_digits
-    chars.find_all { |char| DIGITS.include?(char) }.join
+    string.chars.find_all { |char| DIGITS.include?(char) }.join
   end
 
   def valid_length?
-    digit_count == 10 || digit_count == 11
+    digit_count == 10
   end
 
   def all_chars_valid?
@@ -70,11 +82,11 @@ class PhoneNumberString < String
   end
 
   def digit_count
-    chars.count { |char| DIGITS.include?(char) }
+    string.chars.count { |char| DIGITS.include?(char) }
   end
 
   def first_digit
-    chars.find { |char| DIGITS.include?(char) }
+    string.chars.find { |char| DIGITS.include?(char) }
   end
 end
 
