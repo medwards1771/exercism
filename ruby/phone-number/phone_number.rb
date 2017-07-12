edf2bed1b309cf86f87016cadfe555df08ff8b1f
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-07-12 16:05:36 -0400

    Rely on object class to determine method return

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,29 +1,29 @@
 class PhoneNumber
   def self.clean(string)
-    phone_number = PhoneNumberString.new(string)
-    phone_number.clean if phone_number.valid?
+    if string.digit_count == 11
+      fone_number = ElevenDigitFoneNumber.new(string)
+    elsif string.digit_count == 10
+      fone_number = TenDigitFoneNumber.new(string)
+    else
+      return nil
+    end
+    fone_number.clean if fone_number.valid?
   end
 end
 
-class PhoneNumberString < String
-  PHONE_NUMBER_PUNCTUATION_MARKS = ['(', ')', ' ', '-', '.', '+'].freeze
+class String
   DIGITS = ('0'..'9').to_a
+
+  def digit_count
+    chars.count { |char| DIGITS.include?(char) }
+  end
+end
+
+class FoneNumber < String
+  PHONE_NUMBER_PUNCTUATION_MARKS = ['(', ')', ' ', '-', '.', '+'].freeze
   AREA_CODE_DIGITS = ('2'..'9').to_a
   EXCHANGE_CODE_DIGITS = ('2'..'9').to_a
   VALID_PHONE_NUMBER_CHARS = PHONE_NUMBER_PUNCTUATION_MARKS + DIGITS
-
-  def clean
-    if digit_count == 10
-      just_digits
-    elsif digit_count == 11
-      just_digits[1..-1]
-    end
-  end
-
-  def valid?
-    valid_area_code? && valid_exchange_code? && valid_length? &&
-      all_chars_valid? && valid_first_digit?
-  end
 
   def valid_area_code?
     AREA_CODE_DIGITS.include?(area_code)
@@ -33,48 +33,57 @@ class PhoneNumberString < String
     EXCHANGE_CODE_DIGITS.include?(exchange_code)
   end
 
-  def exchange_code
-    if digit_count == 10
-      just_digits[3]
-    elsif digit_count == 11
-      just_digits[4]
-    end
-  end
-
-  def area_code
-    if digit_count == 10
-      just_digits[0]
-    elsif digit_count == 11
-      just_digits[1]
-    end
-  end
-
-  def valid_first_digit?
-    if digit_count == 11
-      first_digit == '1'
-    elsif digit_count == 10
-      AREA_CODE_DIGITS.include?(first_digit)
-    end
-  end
-
   def just_digits
     chars.find_all { |char| DIGITS.include?(char) }.join
-  end
-
-  def valid_length?
-    digit_count == 10 || digit_count == 11
   end
 
   def all_chars_valid?
     chars.all? { |char| VALID_PHONE_NUMBER_CHARS.include?(char) }
   end
 
-  def digit_count
-    chars.count { |char| DIGITS.include?(char) }
-  end
-
   def first_digit
     chars.find { |char| DIGITS.include?(char) }
+  end
+end
+
+class TenDigitFoneNumber < FoneNumber
+  def clean
+    just_digits
+  end
+
+  def valid?
+    valid_area_code? && valid_exchange_code? && all_chars_valid?
+  end
+
+  def exchange_code
+    just_digits[3]
+  end
+
+  def area_code
+    just_digits[0]
+  end
+end
+
+class ElevenDigitFoneNumber < FoneNumber
+  def clean
+    just_digits[1..-1]
+  end
+
+  def valid?
+    valid_area_code? && valid_exchange_code? && all_chars_valid? &&
+      valid_first_digit?
+  end
+
+  def exchange_code
+    just_digits[4]
+  end
+
+  def area_code
+    just_digits[1]
+  end
+
+  def valid_first_digit?
+    first_digit == '1'
   end
 end
 
