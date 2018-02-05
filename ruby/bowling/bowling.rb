4325629098caf1530f6831474ea28e156ddb70a7
medwards1771 <meredith.edwards1771@gmail.com>
2018-02-05 17:43:00 -0500

    Experiment with OO Bowling

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -1,65 +1,53 @@
 class Game
-  class BowlingError < StandardError
+  attr_writer :score, :running_total
+
+  def initialize
+    @roll = Roll.new
+    @frame = Frame.new
   end
 
   def roll(pins)
-    raise BowlingError if (pins < 0 || pins > 10 || frame_sum > 10)
-    all_pins << pins
+    add_pins_to_running_total(pins)
+    add_pins_to_score(pins)
   end
 
   def score
-    frame_sum = 0
-    total_sum = 0
-    frame = 'uncategorized'
-    frame_index = 1
-    pin_index = 0
-    all_pins.each do |pin|
-      frame_sum += pin
-      total_sum += pin
-      pin_index += 1
-      raise BowlingError if frame_sum > 10
-      if frame_sum == 10 && (frame == 'uncategorized' || frame == 'strike') && frame_index < 10
-        frame = 'strike'
-        frame_index += 1
-        frame_sum = 0
-        total_sum += all_pins[pin_index]
-        total_sum += all_pins[pin_index + 1]
-      elsif frame_sum == 10 && frame_index < 10
-        frame = 'spare'
-        frame_index += 1
-        frame_sum = 0
-        total_sum += all_pins[pin_index]
-      elsif frame_sum == 10 && frame_index == 10
-        frame_sum = 0
-      elsif frame == 'uncategorized' && frame_index == 10
-        frame = 'open_frame'
-      elsif frame == 'uncategorized'
-        frame = 'open_frame'
-      elsif frame == 'open_frame'
-        frame = 'uncategorized'
-        frame_index += 1
-        frame_sum = 0
-      end
+    @score ||= 0
+  end
+
+  def running_total
+    @running_total ||= 0
+  end
+
+  def add_pins_to_score(pins)
+    if @roll.spare
+      self.score = score + pins
+    else
+      self.score = score + pins
     end
-    total_sum
   end
 
-  # still opting to calculate score at the end instead of as you go along
-
-  private
-
-  def all_pins
-    @all_pins ||= []
+  def add_pins_to_running_total(pins)
+    self.running_total = running_total + pins
   end
 
-  def spare?(sum)
-    sum == 10
+  def check_if_spare
+    if running_total == 10
+      @roll.spare = true
+    else
+      @roll.spare = false
+    end
   end
+end
 
-  # the difference between a spare and a strike is 1 and 2 rolls.
-  # sum and pin don't capture this difference very well
+class Roll
+  attr_accessor :spare
+end
 
-  def strike?(pin)
-    pin == 10
+class Frame
+  attr_writer :number
+
+  def number
+    @number ||= 1
   end
 end
