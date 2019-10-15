b4f3cf9c7c6f500dc0c1261108dc7a0bafec51cb
medwards1771 <meredith.edwards1771@gmail.com>
2019-10-15 11:24:17 -0400

    ETL: Write second test and make it pass

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,15 +1,3 @@
-class ETL
-  def self.transform(old_data)
-    new_data = {}
-    old_data.map do |value, letter_array|
-      letter_array.each do |letter|
-        new_data["#{letter.downcase}"] = value
-      end
-    end
-    new_data
-  end
-end
-
-module BookKeeping
-  VERSION = 1
+def score(_)
+  1
 end
--- ruby/etl/etl_test.rb
+++ ruby/etl/etl_test.rb
@@ -3,6 +3,7 @@ require_relative 'etl'
 
 class EtlTest < Minitest::Test
   def test_how_many_points_an_a_is_worth
-    assert_equal "a", 1
+    letter = "a"
+    assert_equal score(letter), 1
   end
 end
