cc8db19ebf7b5b2e1281207255f152a516be4c67
medwards1771 <meredith.edwards1771@gmail.com>
2019-10-15 14:39:31 -0400

    Get rid of overkill tests

--- ruby/etl/etl_test.rb
+++ ruby/etl/etl_test.rb
@@ -7,19 +7,8 @@ class EtlTest < Minitest::Test
     assert_equal score(letter), 1
   end
 
-  def test_how_many_points_b_is_worth
-    letter = "b"
-    assert_equal score(letter), 3
-  end
-
-  def test_how_many_points_any_given_letter_is_worth
-    # is testing all 26 letters actually necessary? what is the risk of not doing it?
-    # i'm tired of typing
-    letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
-    scores = [1, 3, 3, 2, 1, 4, 2, 4]
-
-    letters.each_with_index do |letter, index|
-      assert_equal scores[index], score(letter)
-    end
+  def test_how_many_points_f_is_worth
+    letter = "f"
+    assert_equal score(letter), 4
   end
 end
