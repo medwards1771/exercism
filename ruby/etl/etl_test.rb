bc10010e7eb2e51977b4ed6ae05e20d2d7407acd
medwards1771 <meredith.edwards1771@gmail.com>
2019-10-15 11:31:08 -0400

    Make wording more concise

--- ruby/etl/etl_test.rb
+++ ruby/etl/etl_test.rb
@@ -2,12 +2,12 @@ require 'minitest/autorun'
 require_relative 'etl'
 
 class EtlTest < Minitest::Test
-  def test_how_many_points_an_a_is_worth
+  def test_how_many_points_a_is_worth
     letter = "a"
     assert_equal score(letter), 1
   end
 
-  def test_how_many_points_a_b_is_worth
+  def test_how_many_points_b_is_worth
     letter = "b"
     assert_equal score(letter), 3
   end
