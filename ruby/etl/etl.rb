03a688b1870664e8d439de6c553ef4c09a32bf27
medwards1771 <meredith.edwards1771@gmail.com>
2019-10-16 10:55:07 -0400

    Test total transformation of old into new

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -29,4 +29,35 @@ LETTER_TO_SCORE_MAP = {
 
 def score(letter)
   LETTER_TO_SCORE_MAP[letter]
+  new_system = transform_old_system_into_new_system
+  new_system[letter]
+end
+
+
+def transform_old_system_into_new_system
+  # make a new empty hash
+  new_system = {}
+  # iterate over the old system with k,v
+  old_system.each do |score, letters|
+    # iterate over the v which will be letters
+    letters.each do |letter|
+      # downcase each letter
+      # make each letter a key in the new hash
+      # assign the key's value to the k
+      new_system[letter.downcase] = score
+    end
+  end
+  new_system
+end
+
+def old_system
+  {
+    1 => %w[A E I O U L N R S T],
+    2 => %w[D G],
+    3 => %w[B C M P],
+    4 => %w[F H V W Y],
+    5 => %w[K],
+    8 => %w[J X],
+    10 => %w[Q Z]
+  }
 end
--- ruby/etl/etl_test.rb
+++ ruby/etl/etl_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require_relative 'etl'
+require_relative 'fixtures'
 
 class EtlTest < Minitest::Test
   def test_how_many_points_a_is_worth
@@ -10,5 +11,10 @@ class EtlTest < Minitest::Test
   def test_how_many_points_f_is_worth
     letter = "f"
     assert_equal score(letter), 4
+  end
+
+  def test_letter_to_score_map
+    # for posterity
+    assert_equal LETTER_TO_SCORE_MAP, LETTER_TO_SCORE_MAP_FIXTURE
   end
 end
