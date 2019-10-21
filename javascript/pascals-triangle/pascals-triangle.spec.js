5d1db6b5333f23982329a4152eff5c6499d72cea
medwards1771 <meredith.edwards1771@gmail.com>
2019-10-21 13:01:40 -0400

    Calculate row 1 and 2

--- javascript/pascals-triangle/pascals-triangle.spec.js
+++ javascript/pascals-triangle/pascals-triangle.spec.js
@@ -1,13 +1,21 @@
 class Triangle {
   row(number){
-    return number;
+    if(number === 1){
+      return [1];
+    } else {
+      return [1,1]
+    }
   }
 }
 
 
 describe('#row', ()=> {
   var triangle = new Triangle();
-  it('computes the numbers for a given row', ()=> {
-    expect(triangle.row(1)).toEqual(1)
+  it('computes numbers returned by the first row', ()=> {
+    expect(triangle.row(1)).toEqual([1])
+  });
+
+  it('computes the numbers returned by the second row', ()=> {
+    expect(triangle.row(2)).toEqual([1,1])
   });
 });
