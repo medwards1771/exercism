cb7f6907b933d0c906cadf3bd8b8b17d15fa25cb
medwards1771 <meredith.edwards1771@gmail.com>
2019-10-21 13:30:23 -0400

    WIIIP

--- javascript/pascals-triangle/pascals-triangle.spec.js
+++ javascript/pascals-triangle/pascals-triangle.spec.js
@@ -1,9 +1,22 @@
 class Triangle {
   row(number){
+    var list = [1]
+
     if(number === 1){
-      return [1];
+      return list;
+    } else if(number === 2) {
+      list.push(1);
+      return list
+    } else if(number === 3) {
+      list.push(1);
+      let index = Math.floor(list.length / 2)
+      list.splice(index, 0, 2);
+      return list
     } else {
-      return [1,1]
+      list.push(1);
+      let index = Math.floor(list.length / 2)
+      list.splice(index, 0, 2);
+      return list
     }
   }
 }
@@ -17,5 +30,13 @@ describe('#row', ()=> {
 
   it('computes the numbers returned by the second row', ()=> {
     expect(triangle.row(2)).toEqual([1,1])
+  });
+
+  it('computes the numbers returned by the third row', ()=> {
+    expect(triangle.row(3)).toEqual([1,2,1])
+  });
+
+  it('computes the numbers returned by the fourth row', ()=> {
+    expect(triangle.row(4)).toEqual([1,3,3,1])
   });
 });
